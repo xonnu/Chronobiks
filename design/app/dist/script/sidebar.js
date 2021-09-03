99f6454c7f31c0bbdcc3ae18f19cb03d0306e36b
audaxss <chrono.social@outlook.com>
2021-09-04 01:19:48 +0800

    upde: design side-bar update

--- design/app/dist/script/sidebar.js
+++ design/app/dist/script/sidebar.js
@@ -6,21 +6,32 @@ const closeButton = doc.querySelector('#close-button');
 const sidebar = doc.querySelector('.sidebar');
 const sidebarOverlay = doc.querySelector('.sidebar--overlay');
 
+let delayAdded = 0.3;
 const openSidebar = () => {
     sidebarOverlay.classList.remove('sidebar--overlay--close');
-    sidebar.classList.add('sidebar--open')
-    
-    gsap.from(".sample", {
-         y: 50, 
-         duration: .5, 
-         delay: 0.3, 
-         opacity: 0,
-         ease: "expo. out out"
-    })
+    setTimeout(() => {
+        sidebar.classList.add('sidebar--open')
+        document.querySelectorAll('.sidebar__timeGroup > *').forEach(time => {
+            delayAdded += 0.1
+            gsap.from(time, {
+                y: 30,
+                duration: .3,
+                delay: delayAdded,
+                opacity: 0,
+                ease: "expo. out out",
+                onComplete: () => {
+                    delayAdded = 0.3;
+                }
+            })
+        })
+    }, 250);
 };
 
 const closeSidebar = () => {
-    sidebarOverlay.classList.add('sidebar--overlay--close');
+    setTimeout(() => {
+        sidebarOverlay.classList.add('sidebar--overlay--close');
+    }, 400);
+
     sidebar.classList.remove('sidebar--open');
 };
 
