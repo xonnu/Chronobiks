66e341a8c4d9325b2bbafbbecc3c61c9f1599e84
audaxss <chrono.social@outlook.com>
2021-09-04 16:45:40 +0800

    upde: design interface finished

--- design/app/dist/script/sidebar.js
+++ design/app/dist/script/sidebar.js
@@ -5,13 +5,14 @@ const closeButton = doc.querySelector('#close-button');
 /* design */
 const sidebar = doc.querySelector('.sidebar');
 const sidebarOverlay = doc.querySelector('.sidebar--overlay');
-
+const timeGroup = doc.querySelectorAll('.sidebar__timeGroup > *')
 let delayAdded = 0.3;
+
 const openSidebar = () => {
     sidebarOverlay.classList.remove('sidebar--overlay--close');
     setTimeout(() => {
         sidebar.classList.add('sidebar--open')
-        document.querySelectorAll('.sidebar__timeGroup > *').forEach(time => {
+        timeGroup.forEach(time => {
             delayAdded += 0.1
             gsap.from(time, {
                 y: 30,
@@ -38,3 +39,41 @@ const closeSidebar = () => {
 sidebarOverlay.addEventListener('click', closeSidebar);
 openButton.addEventListener('click', openSidebar);
 closeButton.addEventListener('click', closeSidebar);
+
+
+const navbar = doc.querySelector('.navbar');
+const goTriggered = doc.querySelector('.go');
+const newestTime = doc.querySelector('.timer__newest');
+
+var lastKeyUpAt = 0;
+var isTriggered = false;
+
+const pressVerify = (holdDuration = 1000) => {
+    var keyDownAt = new Date();
+
+    if (isTriggered == true) {
+        isTriggered = false;
+    } else {
+        setTimeout(() => {
+            if (+keyDownAt > +lastKeyUpAt) {
+                newestTime.classList.add('timer__newest--timerRunning');
+                navbar.classList.add('navbar--timerRunning')
+                goTriggered.classList.remove('go--hide');
+                isTriggered = true;
+            } else {
+                isTriggered = false;
+            }
+        }, holdDuration);
+    }
+};
+
+doc.addEventListener('keypress', () => {
+
+    pressVerify()
+})
+
+doc.addEventListener('keyup', () => {
+    goTriggered.classList.add('go--hide');
+    lastKeyUpAt = new Date();
+    isTriggered = false;
+})
